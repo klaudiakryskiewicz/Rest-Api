61fae26a0718bd0a0b62aa88278a208b2e52b10b
klaudiakryskiewicz <klaudia.sandra.kryskiewicz@gmail.com>
2020-08-07 23:01:21 +0200

    heroku war

--- Rest_Api/settings.py
+++ Rest_Api/settings.py
@@ -22,8 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = ')6tr$&-*@^c7cq3c1o^e8wf)togiq%(a5qbje#)=3po!cmj^=)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
